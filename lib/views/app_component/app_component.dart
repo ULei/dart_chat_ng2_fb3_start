fbd72be9bb7dc7dd3fbdb1808798a955b676b712
chris <joanneandy1479@gmail.com>
2017-03-25 22:31:40 +0800

    完成版

--- lib/views/app_component/app_component.dart
+++ lib/views/app_component/app_component.dart
@@ -4,16 +4,34 @@ import 'package:angular2/core.dart';
 
 import '../app_header/app_header.dart';
 import '../../services/firebase_service.dart';
+import '../../directives/vu_scroll_down.dart';
+import '../../directives/vu_hold_focus.dart';
 
 @Component(
     selector: 'my-app',
     templateUrl: 'app_component.html',
-    directives: const [AppHeader],
+    directives: const [AppHeader, VuScrollDown, VuHoldFocus],
     providers: const [FirebaseService],
     styleUrls: const ['app_component.css']
 )
 class AppComponent {
   final FirebaseService fbService;
+  String inputText = "";
 
   AppComponent(FirebaseService this.fbService);
+
+  void sendTextMessage() {
+    String messageText = inputText.trim();
+
+    if (messageText.isNotEmpty) {
+      fbService.sendMessage(text: messageText);
+      inputText = "";
+    }
+  }
+
+  void sendImageMessage(FileList files) {
+    if (files.isNotEmpty) {
+      fbService.sendImage(files.first);
+    }
+  }
 }
